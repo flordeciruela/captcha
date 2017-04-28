09d65a250963a184df5199614a4566a4732238da
Wendy Reyes <wendy.reyessan@gmail.com>
2017-04-28 11:44:30 -0500

    subiendo funcion code

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,11 @@
 var captchaCode = document.getElementById('captcha-code');
 var inputCode= document.getElementById('input-code');
+var captchas = ["A6FT48W","86GH3DK","R0TH3N7","P4UN7V2"];
 
 inputCode.addEventListener('keydown',function(event) {
   if(event.keyCode == 13) {
-		alert("wa")
+		var getCaptcha = Math.floor(Math.random() * captchas.length);
+	   captchaCode.innerText = captchas[getCaptcha];
 	}
+
 });
