603233674ba5ecb7c9f7ce48dc7e094ed84b3e28
Wendy Reyes <wendy.reyessan@gmail.com>
2017-04-28 11:58:30 -0500

    captcha funcionando

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,23 @@
 var captchaCode = document.getElementById('captcha-code');
 var inputCode= document.getElementById('input-code');
 var captchas = ["A6FT48W","86GH3DK","R0TH3N7","P4UN7V2"];
+var load = document.getElementById('load');
+var enter = document.getElementById('enter');
 
-inputCode.addEventListener('keydown',function(event) {
-  if(event.keyCode == 13) {
-		var getCaptcha = Math.floor(Math.random() * captchas.length);
-	   captchaCode.innerText = captchas[getCaptcha];
+window.addEventListener('load',function(event) {
+	var getCaptcha = Math.floor(Math.random() * captchas.length);
+   captchaCode.innerText = captchas[getCaptcha];
+});
+
+load.addEventListener('click',function(event) {
+	var getCaptcha = Math.floor(Math.random() * captchas.length);
+	captchaCode.innerText = captchas[getCaptcha];
+});
+
+enter.addEventListener('click',function(event) {
+	if (inputCode.value == captchaCode.innerText) {
+		alert("Validaciòn exitosa!");
+	} else {
+		alert("Captcha incorrecto, intente nuevamente.");
 	}
-
 });
