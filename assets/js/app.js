2b972e0e7db1a928af48bd1f8ee8a6977f9aefad
flordeciruela <wendy.reyessan@gmail.com>
2017-04-28 06:21:59 -0500

    subiendo maquetacion

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+var captchaCode = document.getElementById('captcha-code');
+var inputCode= document.getElementById('input-code');
+
+inputCode.addEventListener('keydown',function(event) {
+  if(event.keyCode == 13) {
+		alert("wa")
+	}
+});
