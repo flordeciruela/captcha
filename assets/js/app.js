d9eedcd04ed9e766203e397463af693fb424f824
Wendy Reyes <wendy.reyessan@gmail.com>
2017-05-01 15:21:04 -0500

    js refactorizado

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,42 +1,47 @@
 var appCaptcha = document.getElementById('captcha');
 var captchaCode = document.getElementById('captcha-code');
 var inputCode= document.getElementById('input-code');
-var captchas = ["A6FT48W","86GH3DK","R0TH3N7","P4UN7V2"];
-var load = document.getElementById('load');
+var reload = document.getElementById('reload');
 var enter = document.getElementById('enter');
 
 window.addEventListener('load',function(event) {
 	generarCaptcha();
-	colorAleatorio();
 });
 
-load.addEventListener('click',function(event) {
+reload.addEventListener('click',function(event) {
 	event.preventDefault();
 	generarCaptcha();
-	colorAleatorio();
 });
 
 enter.addEventListener('click',function(event) {
 	event.preventDefault();
 	if (inputCode.value == captchaCode.innerText) {
-		alert("Validaciòn exitosa!");
+		alert("Validación exitosa!");
 		appCaptcha.reset();
 	} else {
 		alert("Error: ingresar código de la imagen (distingue entre mayúsculas y minúsculas).");
 		appCaptcha.reset();
+		generarCaptcha(); //Cambia el código captcha para el nuevo intento.
 	}
 });
 
 function colorAleatorio() {
-    var letras = "0123456789ABCDEF";
-    var color = '#';
-    for (var i = 0; i < 6; i++ ) {
-        color += letras[Math.floor(Math.random() * 16)];
-    }
-		captchaCode.style.color=color;
+  var caracteres = "0123456789ABCDEF";
+  var color = "#";
+	for (var i = 0; i < 6; i++ ) {
+    color += caracteres[Math.floor(Math.random() * caracteres.length)];
+  }
+	captchaCode.style.color=color;
 }
 
 function generarCaptcha() {
-	var nuevoCaptcha = Math.floor(Math.random() * captchas.length);
-	captchaCode.innerText = captchas[nuevoCaptcha];
+	var letrasCaptcha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'";
+	var numCaptcha = "0123456789";
+	captchaCode.innerText = letrasCaptcha[Math.floor(Math.random() * letrasCaptcha.length)];
+	//bucle que alterna entre letras y numeros para generar el código.
+	while (captchaCode.innerText.length < 6) {
+		captchaCode.innerText += numCaptcha[Math.floor(Math.random() * numCaptcha.length)] +
+		letrasCaptcha[Math.floor(Math.random() * letrasCaptcha.length)];
+	}
+	colorAleatorio();
 }
